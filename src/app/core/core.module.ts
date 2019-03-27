dd613698912129294576fd0aa2c971f2766416fe
David <davidfloyd322@gmail.com>
2019-03-27 14:53:49 +0200

    feature/app, add materialUI animation

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,12 +2,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
     declarations: [],
     imports: [ 
         CommonModule,
-        HttpClientModule
+        HttpClientModule,
+        BrowserAnimationsModule
     ],
 })
 export class CoreModule {}
