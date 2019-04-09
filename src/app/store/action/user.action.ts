3829c47dbdfbfe8db8c6ca777c5a3b997a81354d
David <davidfloyd322@gmail.com>
2019-04-09 16:55:24 +0300

    fix/state, fix state for user

--- src/app/store/action/user.action.ts
+++ src/app/store/action/user.action.ts
@@ -1,11 +1,19 @@
 import { Action } from '@ngrx/store';
+import { IUser } from 'src/app/shared/interfaces/user';
 
 export enum EUserActions {
     GetUser = '[User] Get User',
+    GetUserSucces = '[User] Get User Succes'
 }
 
 export class GetUser implements Action {
     public readonly type = EUserActions.GetUser;
+    constructor (public payload: string) {}
 }
 
-export type UserAction = GetUser; 
+export class GetUserSucces implements Action {
+    public readonly type = EUserActions.GetUserSucces;
+    constructor (public payload: IUser) {}
+}
+
+export const UserAction = GetUser ; 
