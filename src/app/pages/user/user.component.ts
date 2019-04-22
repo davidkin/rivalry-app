780e80b1658153f548613f796e391cf0db7ac45b
David <davidfloyd322@gmail.com>
2019-04-22 16:16:17 +0300

    feature/useStore, use info about user from store

--- src/app/pages/user/user.component.ts
+++ src/app/pages/user/user.component.ts
@@ -3,21 +3,40 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { IUser } from 'src/app/shared/interfaces/user';
 import { UserService } from './user.service';
 import { IRepos } from 'src/app/shared/interfaces/repos';
+import { selectSelectedUser } from 'src/app/store/selectors/user.selector';
+import { IAppState } from 'src/app/store/state/app.state';
+import { Store, select } from '@ngrx/store';
+import { GetUser } from 'src/app/store/action/user.action';
+import { Observable } from 'rxjs';
+import { IUserState } from 'src/app/store/state/user.state';
+
 @Component({
     selector: 'user',
     templateUrl: './user.component.html',
     styleUrls: ['./user.component.scss']
 })
 export class UserComponent implements OnInit {
+    user$: Observable<IUserState> = this._store.pipe(select(selectSelectedUser));
     user: IUser;
     repos: IRepos;
-    constructor( private route: ActivatedRoute, private userService: UserService, private router: Router) { }
+
+    constructor( 
+        private route: ActivatedRoute, 
+        private userService: UserService, 
+        private router: Router,
+        private _store: Store<IAppState>
+    ) { }
 
     ngOnInit(): void { 
-        this.user = this.route.snapshot.data.userData; 
+        console.log('--- UserComponent (user$)', this.user$);
+
+        this.user$.subscribe(data => this.user = data.user);
+        
+        // this.user = this.route.snapshot.data.userData; 
+        // this._store.dispatch(new GetUser(this.route.snapshot.data.userData))
     }
 
-    getRepos(): void{
+    getRepos(): void {
         this.userService.getUserRepos(this.user.login).subscribe(repos => {
             this.repos = repos;
             this.router.navigate([`user/${this.user.login}/repos`]);
