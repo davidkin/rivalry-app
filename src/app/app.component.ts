dcd62069ac0e8c98c4493048cd0a46c94052927d
David <davidfloyd322@gmail.com>
2019-04-22 13:48:56 +0300

    feature/ngrx, add main ngrx modules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store, select } from '@ngrx/store';
+
+import { IAppState } from './store/state/app.state';
+import { GetRepos } from './store/action/repos.action';
+import { selectRepos } from './store/selectors/repos.selectors';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'rivalry-app';
+  config$ = this._store.pipe(select(selectRepos));
+
+  constructor(private _store: Store<IAppState>){}
+
+  ngOnInit() {
+    this._store.dispatch(new GetRepos());
+  }
 }
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,14 +2,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StoreModule } from '@ngrx/store';
+import { appReducer } from '../store/reducers/app.reducer';
+import { EffectsModule } from '@ngrx/effects';
+import { UserEffects } from '../store/effects/user.effects';
+import { ReposEffects } from '../store/effects/repos.effects';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
     declarations: [],
     imports: [ 
         CommonModule,
         HttpClientModule,
-        BrowserAnimationsModule
+        BrowserAnimationsModule,
+        StoreModule.forRoot(appReducer),
+        EffectsModule.forRoot([ UserEffects,ReposEffects ]),
+        StoreRouterConnectingModule.forRoot({ stateKey: 'router' }),
     ],
 })
 export class CoreModule {}
